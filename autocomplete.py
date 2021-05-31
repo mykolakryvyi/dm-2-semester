ac96f277ff14e95241fe0ed754b38f0ae2fb4597
Alina Voronina <alina.voronina@ucu.edu.ua>
2021-05-31 18:34:45 +0300

    changed autocomplete.py

--- autocomplete.py
+++ autocomplete.py
@@ -78,34 +78,3 @@ def start_completing():
         except ValueError:
             continue
     return search_trie
-
-
-# app = Flask(__name__)
-# @app.route("/", methods=["GET","POST"])
-# def index():
-#     if request.method == "POST":
-#         msg = request.form.get('msg')
-#         print(msg)
-#         trie = start_completing()
-#         return str(Autocompleter(trie).sorted_autocomplete('hell'))
-
-#     return render_template('index.html', words = str(Autocompleter(start_completing()).sorted_autocomplete('hell')))
-def plot_ngram():
-    '''
-    Return url to the google ngram plot.
-    '''
-    words = ['motive', 'sunlight', 'railway', 'abroad', 'supper']
-    query_str = '%2C'.join(words)
-    base_url = f'https://books.google.com/ngrams/interactive_chart?content=\
-{query_str}&year_start=1800&year_end=2000&corpus=15&smoothing=3'
-    return base_url
-
-app = Flask(__name__)
-
-@app.route("/", methods=["GET","POST"])
-def index():
-    data = plot_ngram()
-    return render_template('index.html', data=data)
-
-if __name__ == "__main__":
-    app.run(debug=True)
